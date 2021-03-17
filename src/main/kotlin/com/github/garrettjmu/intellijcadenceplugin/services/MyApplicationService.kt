37c3bb8bf0e1c853adefdc81893e3884fe17d497
GitHub Action <action@github.com>
2021-03-17 14:42:09 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/garrettjmu/intellijcadenceplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.garrettjmu.intellijcadenceplugin.services
+
+import com.github.garrettjmu.intellijcadenceplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
